9f4ca750a726844d909b097165f7d07e3539cb66
gerardo-garcia <gerardo-garcia@users.noreply.github.com>
2023-05-08 18:26:42 +0200

    Add first version of the program, requirements and tox.ini
    Signed-off-by: gerardo-garcia <gerardo-garcia@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # lldp-topo
-Program that crates a table of links between servers and switches obtained via SSH+LLDP
+
+This program takes as input a list of nodes to be connected via ssh (pubkey has been injected in the node) and provides as output a table with the list of links, printing them in a table or CSV format.
+
+The program assumes that you can access via ssh to the list of nodes/servers and they have lldpd running (the command lldpcli must be available).
+
+## Getting started
+
+```bash
+$ ./lldp_topo.py -h
+usage: lldp_topo.py [-h] [-o {table,csv,yaml,json}] [--test] [-v] SERVER [SERVER ...]
+
+positional arguments:
+  SERVER                server to be connected in format `user@server`
+
+options:
+  -h, --help            show this help message and exit
+  -o {table,csv,yaml,json}, --output {table,csv,yaml,json}
+                        output format
+  --test                only test ssh connectivity and lldpcli
+  -v, --verbose         increase output verbosity
+```
+
+To get LLDP topology from a list of servers:
+
+```bash
+$ ./lldp_topo.py user1@server1 user2@server2
++------------+--------+------------+-------------------+----------------+--------+---------------------------+
+| Edge 1     | BRWS 1 | Iface 1    | MAC 1             | Edge 2         | BRWS 2 | Iface 2                   |
++------------+--------+------------+-------------------+----------------+--------+---------------------------+
+| server1    | 1100   | ens3f0     | 68:05:ca:38:6c:50 | None           | None   | None                      |
+| server1    | 1100   | eno1       | d0:67:26:cc:f0:ea | switch1        | 11XX   | Ten-GigabitEthernet2/0/9  |
+| server1    | 1100   | ens3f1     | 68:05:ca:38:6c:51 | None           | None   | None                      |
+| server1    | 1100   | ens3f2     | 68:05:ca:38:6c:52 | None           | None   | None                      |
+| server1    | 1100   | eno2       | d0:67:26:cc:f0:eb | switch1        | 11XX   | Ten-GigabitEthernet2/0/10 |
+| server1    | 1100   | ens3f3     | 68:05:ca:38:6c:53 | None           | None   | None                      |
+| server1    | 1100   | eno3       | d0:67:26:cc:f0:ec | switch1        | 11XX   | Ten-GigabitEthernet2/0/11 |
+| server1    | 1100   | eno4       | d0:67:26:cc:f0:ed | switch1        | 11XX   | Ten-GigabitEthernet2/0/12 |
+| server2    | 0001   | eno1       | d0:67:26:cc:f5:94 | switch1        | 11XX   | Ten-GigabitEthernet2/0/5  |
+| server2    | 0001   | ens3f0     | 3c:fd:fe:9d:40:e8 | None           | None   | None                      |
+| server2    | 0001   | eno2       | d0:67:26:cc:f5:95 | switch1        | 11XX   | Ten-GigabitEthernet2/0/6  |
+| server2    | 0001   | ens3f1     | 3c:fd:fe:9d:40:e9 | None           | None   | None                      |
+| server2    | 0001   | ens3f2     | 3c:fd:fe:9d:40:ea | None           | None   | None                      |
+| server2    | 0001   | eno3       | d0:67:26:cc:f5:96 | switch1        | 11XX   | Ten-GigabitEthernet2/0/7  |
+| server2    | 0001   | ens3f3     | 3c:fd:fe:9d:40:eb | None           | None   | None                      |
+| server2    | 0001   | eno4       | d0:67:26:cc:f5:97 | switch1        | 11XX   | Ten-GigabitEthernet2/0/8  |
++------------+--------+------------+-------------------+----------------+--------+---------------------------+
+
+$ ./lldp_topo.py -o csv user1@server1 user2@server2
+Edge 1;BRWS 1;Iface 1;MAC 1;Edge 2;BRWS 2;Iface 2
+server1;1100;ens3f0;68:05:ca:38:6c:50;None;None;None
+server1;1100;eno1;d0:67:26:cc:f0:ea;switch1;11XX;Ten-GigabitEthernet2/0/9
+server1;1100;ens3f1;68:05:ca:38:6c:51;None;None;None
+server1;1100;ens3f2;68:05:ca:38:6c:52;None;None;None
+server1;1100;eno2;d0:67:26:cc:f0:eb;switch1;11XX;Ten-GigabitEthernet2/0/10
+server1;1100;ens3f3;68:05:ca:38:6c:53;None;None;None
+server1;1100;eno3;d0:67:26:cc:f0:ec;switch1;11XX;Ten-GigabitEthernet2/0/11
+server1;1100;eno4;d0:67:26:cc:f0:ed;switch1;11XX;Ten-GigabitEthernet2/0/12
+server2;0001;eno1;d0:67:26:cc:f5:94;switch1;11XX;Ten-GigabitEthernet2/0/5
+server2;0001;ens3f0;3c:fd:fe:9d:40:e8;None;None;None
+server2;0001;eno2;d0:67:26:cc:f5:95;switch1;11XX;Ten-GigabitEthernet2/0/6
+server2;0001;ens3f1;3c:fd:fe:9d:40:e9;None;None;None
+server2;0001;ens3f2;3c:fd:fe:9d:40:ea;None;None;None
+server2;0001;eno3;d0:67:26:cc:f5:96;switch1;11XX;Ten-GigabitEthernet2/0/7
+server2;0001;ens3f3;3c:fd:fe:9d:40:eb;None;None;None
+server2;0001;eno4;d0:67:26:cc:f5:97;switch1;11XX;Ten-GigabitEthernet2/0/8
+```
+
+To test SSH access to the machines and check that LLDP works in that machine, run:
+
+```bash
+$ ./lldp_topo.py --test user1@server1 user2@server2
+Server user1@server1: SSH working
+Server user1@server1: LLDPCLI working
+Server user2@server2: SSH working
+Server user2@server2: LLDPCLI working
+```
+
+## Requirements
+
+- Python3
---
+++ lldp_topo.py
@@ -0,0 +1,303 @@
+#!/usr/bin/python3
+
+import json
+import yaml
+import argparse
+from prettytable import PrettyTable
+import logging
+import paramiko
+import subprocess
+
+
+####################################
+# Global functions
+####################################
+def print_pretty_table(headers, rows):
+    table = PrettyTable(headers)
+    for row in rows:
+        table.add_row(row)
+    table.align = "l"
+    print(table)
+
+
+def print_yaml_json(headers, rows, to_json=False):
+    output = []
+    for row in rows:
+        item = {}
+        for i in range(len(row)):
+            item[headers[i]] = row[i]
+        output.append(item)
+    if to_json:
+        print(json.dumps(output, indent=4))
+    else:
+        print(yaml.safe_dump(output, indent=4, default_flow_style=False, sort_keys=False))
+
+
+def print_csv(headers, rows):
+    print(";".join(headers))
+    for row in rows:
+        str_row = list(map(str, row))
+        print(";".join(str_row))
+
+
+def print_table(headers, rows, output_format):
+    if output_format == "table":
+        print_pretty_table(headers, rows)
+    elif output_format == "yaml":
+        print_yaml_json(headers, rows)
+    elif output_format == "json":
+        print_yaml_json(headers, rows, to_json=True)
+    else:
+        # if output_format == "csv":
+        print_csv(headers, rows)
+
+
+def set_logger(verbose):
+    global logger
+    log_format_simple = "%(levelname)s %(message)s"
+    log_format_complete = "%(asctime)s %(levelname)s %(name)s %(filename)s:%(lineno)s %(funcName)s(): %(message)s"
+    log_formatter_simple = logging.Formatter(log_format_simple, datefmt="%Y-%m-%dT%H:%M:%S")
+    handler = logging.StreamHandler()
+    handler.setFormatter(log_formatter_simple)
+    logger = logging.getLogger("lldp-topo")
+    logger.setLevel(level=logging.WARNING)
+    logger.addHandler(handler)
+    if verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    elif verbose > 1:
+        log_formatter = logging.Formatter(log_format_complete, datefmt="%Y-%m-%dT%H:%M:%S")
+        handler.setFormatter(log_formatter)
+        logger.setLevel(level=logging.DEBUG)
+
+
+def get_replicas(manifest, kind):
+    # Get replicas, replicas_str
+    if kind in {"DaemonSet"}:
+        replicas = 1
+        replicas_str = "N/A (auto)"
+    elif kind in {"Pod"}:
+        replicas = 1
+        replicas_str = "1 (auto)"
+    elif kind in {"Deployment", "StatefulSet", "ReplicaSet"}:
+        replicas = manifest.get("spec", {}).get("replicas")
+        if not replicas:
+            replicas = 1
+            replicas_str = "1 (auto)"
+        else:
+            replicas_str = str(replicas)
+        logger.info(f"Replicas: {replicas}")
+    return replicas, replicas_str
+
+
+def run_command(server, command, ssh_command=None):
+    if not ssh_command:
+        logger.info(f"Server: {server}")
+        server_fields = server.split("@")
+        username = server_fields[0]
+        host = server_fields[1]
+        client = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username=username)
+        logger.info(f"Command: {command}")
+        _stdin, _stdout, _stderr = client.exec_command(command)
+        logger.debug(f"Command: {command} completed")
+        # print(_stdout.read().decode())
+    else:
+        result = subprocess.run(
+            ["/bin/sh", "-c", f"{ssh_command}", f"{server}", f"{command}"],
+            capture_output=True,
+            text=True,
+        )
+        _stdout = result.stdout
+        # _stderr = result.stderr
+    return _stdout.read().decode()
+
+
+def test_ssh_lldpcli(servers_list, ssh_command=None):
+    test_output = 0
+    for server in servers_list:
+        command = "echo"
+        try:
+            run_command(server, command, ssh_command)
+            print(f"Server {server}: SSH working")
+            command = "lldpcli show chassis"
+            try:
+                run_command(server, command, ssh_command)
+                print(f"Server {server}: LLDPCLI working")
+            except Exception:
+                test_output = 1
+                print(f"Server {server}: LLDPCLI NOT working")
+        except Exception:
+            test_output = 1
+            print(f"Server {server}: SSH NOT working")
+    return test_output
+
+
+def get_lldp_info(server, ssh_command=None):
+    try:
+        command = "lldpcli -f json show chassis details"
+        output = run_command(server, command, ssh_command)
+        chassis = yaml.safe_load(output)
+        logger.debug(f"Server {server}. Chassis: {chassis}")
+        command = "lldpcli -f json show interfaces details"
+        output = run_command(server, command, ssh_command)
+        interfaces = yaml.safe_load(output)
+        logger.debug(f"Server {server}. Interfaces: {interfaces}")
+        command = "lldpcli -f json show neighbors details"
+        output = run_command(server, command, ssh_command)
+        neighbors = yaml.safe_load(output)
+        logger.debug(f"Server {server}. Neighbors: {neighbors}")
+    except Exception:
+        logger.info(print(f"Server {server}: {command} DID NOT WORK"))
+    return chassis, interfaces, neighbors
+
+
+def get_brws_capabilities(chassis):
+    logger.debug(f"Capabilities: {yaml.safe_dump(chassis, indent=4, default_flow_style=False, sort_keys=False)}")
+    bridge = "X"
+    router = "X"
+    wlan = "X"
+    station = "X"
+    for c in chassis.get("capability", []):
+        if c.get("type") == "Bridge":
+            if "enabled" in c:
+                bridge = str(int(c.get("enabled")))
+            continue
+        if c.get("type") == "Router":
+            if "enabled" in c:
+                router = str(int(c.get("enabled")))
+            continue
+        if c.get("type") == "Wlan":
+            if "enabled" in c:
+                wlan = str(int(c.get("enabled")))
+            continue
+        if c.get("type") == "Station":
+            if "enabled" in c:
+                station = str(int(c.get("enabled")))
+            continue
+    brws = f"{bridge}{router}{wlan}{station}"
+    return brws
+
+
+def get_neighbors(neighbors):
+    neighbor_dict = {}
+    for neigh in neighbors.get("lldp", {}).get("interface", []):
+        for iface_name, iface_info in neigh.items():
+            chassis = iface_info.get("chassis", {})
+            chassis_keys = list(chassis.keys())
+            if len(chassis_keys) != 1:
+                logger.error(f"Expected a single chassis in iface info. There are {len(chassis_keys)} keys: {chassis_keys}")
+                exit(1)
+            chassis_name = chassis_keys[0]
+            brws = get_brws_capabilities(chassis[chassis_name])
+            port = iface_info.get("port", {}).get("id", {}).get("value", "")
+            neighbor_dict[iface_name] = {
+                "chassis": chassis_name,
+                "brws": brws,
+                "port": port,
+            }
+    logger.info(f"Neighbor info:\n{yaml.safe_dump(neighbor_dict, indent=4, default_flow_style=False, sort_keys=False)}")
+    return neighbor_dict
+
+
+####################################
+# Global variables
+####################################
+topo = {}
+
+####################################
+# Main
+####################################
+if __name__ == "__main__":
+    # Argument parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        choices=["table", "csv", "yaml", "json"],
+        default="table",
+        help="output format",
+    )
+    parser.add_argument(
+        "--test",
+        default=False,
+        action="store_true",
+        help="only test ssh connectivity and lldpcli",
+    )
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
+    parser.add_argument(
+        "servers_list",
+        metavar="SERVER",
+        type=str,
+        nargs="+",
+        help="server to be connected in format `user@server`",
+    )
+    args = parser.parse_args()
+
+    # Initialize logger
+    set_logger(args.verbose)
+
+    # Initialize variables
+    headers = [
+        "Edge 1",
+        "BRWS 1",
+        "Iface 1",
+        "MAC 1",
+        "Edge 2",
+        "BRWS 2",
+        "Iface 2",
+    ]
+    rows = []
+
+    if args.test:
+        # Test SSH and LLDPCLI and exits
+        test_output = test_ssh_lldpcli(args.servers_list)
+        exit(test_output)
+    for server in args.servers_list:
+        # Get LLDP info and append it to rows
+        logger.info(f"Server: {server}")
+        chassis, interfaces, neighbors = get_lldp_info(server)
+        logger.debug(f"Chassis: {yaml.safe_dump(chassis, indent=4, default_flow_style=False, sort_keys=False)}")
+
+        # Get relevant fields from chassis
+        chassis_keys = list(chassis.get("local-chassis", {}).get("chassis", {}).keys())
+        if len(chassis_keys) != 1:
+            logger.error(f"More than 1 chassis found in server {server}")
+            exit(1)
+        edge1 = chassis_keys[0]
+        brws1 = get_brws_capabilities(chassis["local-chassis"]["chassis"][edge1])
+
+        # Get relevant fields from neighbors and store them in neighbors_dict[iface]
+        logger.debug(f"Neighbors: {yaml.safe_dump(neighbors, indent=4, default_flow_style=False, sort_keys=False)}")
+        neighbors_dict = get_neighbors(neighbors)
+
+        # Iterate over interfaces and get relevant fields from interfaces and the associated neighbor
+        logger.debug(f"Interfaces: {yaml.safe_dump(interfaces, indent=4, default_flow_style=False, sort_keys=False)}")
+        interface_list = interfaces.get("lldp", {}).get("interface", [])
+        for iface in interface_list:
+            iface_keys = list(iface.keys())
+            if len(chassis_keys) != 1:
+                logger.error(f"Expected a single key in iface dict. There are {len(chassis_keys)} keys: {iface_keys}")
+                exit(1)
+            # Get relevant fields from interface
+            iface1 = iface_keys[0]
+            mac1 = iface[iface1].get("port", {}).get("id", {}).get("value", "")
+            # Get relevant fields from neighbors in interface iface1
+            neighbor_info = neighbors_dict.get(iface1, {})
+            edge2 = neighbor_info.get("chassis")
+            brws2 = neighbor_info.get("brws")
+            iface2 = neighbor_info.get("port")
+
+            rows.append(
+                [
+                    edge1,
+                    brws1,
+                    iface1,
+                    mac1,
+                    edge2,
+                    brws2,
+                    iface2,
+                ]
+            )
+
+    print_table(headers, rows, args.output)
